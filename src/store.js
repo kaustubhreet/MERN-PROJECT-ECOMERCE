479f2c92b54046a478ce2383b70f1cf1b94ffbf6
kaustubhreet <mishuyashu23@gmail.com>
2021-10-31 13:11:51 +0530

    10/31/2021

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import {createStore,combineReducers, applyMiddleware,compose} from 'redux';
-import {productListReducer} from './reducers/productReducers';
+import {productListReducer,productDetailsReducer} from './reducers/productReducers';
 import thunk from 'redux-thunk';
 
 const intitialState={};
 const reducer=combineReducers({
     productList:productListReducer,
+    productDetails:productDetailsReducer
 })
 const composeEnhancer=window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store=createStore(reducer,intitialState,composeEnhancer(applyMiddleware(thunk)));
